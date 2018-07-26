e26d391f39b5f8e13779004e33f2ffcb793c4f9a
Tronze <0183535663@naver.com>
2018-07-26 15:05:46 +0000

    Tasks added to manage in admin

--- admin.py
+++ admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import Information as ProjectInformation
+from .models import Task as ProjectTask
 
 # Register your models here.
 admin.site.register(ProjectInformation)
+admin.site.register(ProjectTask)
